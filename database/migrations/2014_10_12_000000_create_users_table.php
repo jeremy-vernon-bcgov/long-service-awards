2a711dc58b63faf7d99bf3e43a7afa213ea25ff4
TVWerdal <thayne.werdal@gov.bc.ca>
2021-06-17 14:13:57 -0700

    Update to trusted proxies

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('role')->default('4'); // TODO: work around to role issue on user create. Remove.
+            $table->string('role');
             $table->foreignId('organization_id')->nullable()->constrained();
             $table->rememberToken();
             $table->timestamps();
